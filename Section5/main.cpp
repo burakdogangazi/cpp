b0ad215936f2f3ad77e8666f8f5e6d0c4edd94ce
“Burak <ddoganbburak@gmail.com>
2024-07-30 23:34:29 +0300

    Preprocess

---
+++ Section5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+/*
+ * Preprocesseor directrives means it that processes your source code before the compiler sees it.
+ * ''#'' means directives
+ * The c++ preprocessor does not understand c++ it simply follows the preprocessor directives and gets the source code ready for the compiler
+ * 90 keyword in c++, java has 50, c has 32 python has 32
+ */
